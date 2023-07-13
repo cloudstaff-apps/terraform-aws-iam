e52f9228aa5ec7be0065929d46feb983d9e1f7be
Nitin Bhadauria <nitinb@cloudstaff.com>
2023-07-13 13:47:58 +1000

    add module

---
+++ modules/iam-ecs-task-role/main.tf
@@ -0,0 +1,63 @@
+resource "aws_iam_role" "this" {
+  count = var.create_role ? 1 : 0
+
+  name                 = var.role_name
+  name_prefix          = var.role_name_prefix
+  tags                 = var.tags
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+data "aws_iam_policy_document" "ecs_s3_policy_doc" {
+  count = var.create_role && length(var.s3_access) > 0 ? 0 : 1
+
+  dynamic "statement" {
+    for_each = var.allow_self_assume_role ? [1] : []
+
+    content {
+      actions = ["s3:PutObject", "s3:PutObjectAcl", "s3:GetObject", "s3:DeleteObject"]
+      resources = var.s3_access
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_s3_policy" {
+  count  = var.create_role && length(var.s3_access) > 0 ? 0 : 1
+  name   = "${var.role_name}-s3-policy"
+  role   = aws_iam_role.this[0].name
+
+  policy = data.aws_iam_policy_document.ecs_s3_policy_doc.this[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  role       = aws_iam_role.this[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "custom" {
+  count = var.create_role ? coalesce(var.number_of_custom_role_policy_arns, length(var.custom_role_policy_arns)) : 0
+
+  role       = aws_iam_role.this[0].name
+  policy_arn = element(var.custom_role_policy_arns, count.index)
+}
+
+resource "aws_iam_role_policy_attachment" "readonly" {
+  count = var.create_role && var.attach_readonly_policy ? 1 : 0
+
+  role       = aws_iam_role.this[0].name
+  policy_arn = var.readonly_role_policy_arn
+}
---
+++ modules/iam-ecs-task-role/variables.tf
@@ -0,0 +1,83 @@
+variable "create_role" {
+  description = "Whether to create a role"
+  type        = bool
+  default     = false
+}
+
+variable "role_name" {
+  description = "IAM role name"
+  type        = string
+  default     = null
+}
+
+variable "role_name_prefix" {
+  description = "IAM role name prefix"
+  type        = string
+  default     = null
+}
+
+variable "role_path" {
+  description = "Path of IAM role"
+  type        = string
+  default     = "/"
+}
+
+variable "tags" {
+  description = "A map of tags to add to IAM role resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "custom_role_policy_arns" {
+  description = "List of ARNs of IAM policies to attach to IAM role"
+  type        = list(string)
+  default     = []
+}
+
+variable "s3_access" {
+  description = "List of ARNs of resources"
+  type        = list(string)
+  default     = []
+}
+
+variable "s3_readonly_access" {
+  description = "List of ARNs of resources"
+  type        = list(string)
+  default     = []
+}
+
+variable "sqs_access" {
+  description = "List of ARNs of resources"
+  type        = list(string)
+  default     = []
+}
+
+variable "sqs_readonly_access" {
+  description = "List of ARNs of resources"
+  type        = list(string)
+  default     = []
+}
+
+variable "number_of_custom_role_policy_arns" {
+  description = "Number of IAM policies to attach to IAM role"
+  type        = number
+  default     = null
+}
+
+variable "readonly_role_policy_arn" {
+  description = "Policy ARN to use for readonly role"
+  type        = string
+  default     = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+variable "attach_readonly_policy" {
+  description = "Whether to attach a readonly policy to a role"
+  type        = bool
+  default     = false
+}
+
+variable "role_description" {
+  description = "IAM Role description"
+  type        = string
+  default     = ""
+}
